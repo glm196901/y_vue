5405e57fb8efb56feb1129afadfd9d3f3e3c0926
worldfilm <guo565656@gmail.com>
2019-05-22 17:38:58 +0800

    no message

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -13,14 +13,15 @@
 import HeaderBar from '@/components/HeaderBar.vue'//头部
 import Footer from '@/components/Footer.vue'//脚部
 import Banner from '@/components/Banner.vue'//轮播图
-import JumpArea from '@/components/JumpArea.vue'
-import ShowInfoBar from '@/components/ShowInfoBar.vue'
-import WinInfo from '@/components/WinInfo.vue'
-import GameList from '@/components/GameList.vue'//
+import JumpArea from '@/components/JumpArea.vue'//导航
+import ShowInfoBar from '@/components/ShowInfoBar.vue'//滚动条
+import WinInfo from '@/components/WinInfo.vue'//中奖信息
+import GameList from '@/components/GameList.vue'//游戏列表
 export default {
   name: 'Home',
   data () {
     return {
+
     }
   },
   components:{
